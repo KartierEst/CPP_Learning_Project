8ad94d4586f422ec1621d7b77ce15a7cfcec289b
abder <rachidmavou@gmail.com>
2022-03-11 18:29:07 +0100

    task 0 update

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/abder/CLionProjects/CPP_Learning_Project
+# Install script for directory: /home/abder/Bureau/m1/c++/CPP_Learning_Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/abder/CLionProjects/CPP_Learning_Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/abder/Bureau/m1/c++/CPP_Learning_Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
